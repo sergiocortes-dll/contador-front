89be8aa28a7fa3bd141d8870b4e3fcf08e93eb88
sergiocortes-dll <contacto.sergiocortes@gmail.com>
2025-12-02 14:40:02 -0500

    announcement fix

--- src/components/saved-counters.tsx
+++ src/components/saved-counters.tsx
@@ -1,36 +1,33 @@
-import React from "react";
-import type { CounterWithCount } from "../types";
-
 function SavedCounter() {
-  const [counters, setCounters] = React.useState<CounterWithCount[]>([]);
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState<string | unknown>("");
-  const [refreshTrigger, setRefreshTrigger] = React.useState(0);
+  // const [counters, setCounters] = React.useState<CounterWithCount[]>([]);
+  // const [loading, setLoading] = React.useState(true);
+  // const [error, setError] = React.useState<string | unknown>("");
+  // const [refreshTrigger, setRefreshTrigger] = React.useState(0);
 
-  const getCountersWithCount = React.useCallback(async () => {
-    try {
-      setError("");
-      setLoading(true);
-      const res = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}api/counter/with-count`
-      );
-      const data = await res.json();
-      setCounters(data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  // const getCountersWithCount = React.useCallback(async () => {
+  //   try {
+  //     setError("");
+  //     setLoading(true);
+  //     const res = await fetch(
+  //       `${import.meta.env.VITE_API_BASE_URL}api/counter/with-count`
+  //     );
+  //     const data = await res.json();
+  //     setCounters(data);
+  //   } catch (err) {
+  //     setError(err);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, []);
 
-  React.useEffect(() => {
-    getCountersWithCount();
-  }, [getCountersWithCount, refreshTrigger]); // Añadimos refreshTrigger como dependencia
+  // React.useEffect(() => {
+  //   getCountersWithCount();
+  // }, [getCountersWithCount, refreshTrigger]); // Añadimos refreshTrigger como dependencia
 
   // Función para refrescar los contadores desde los componentes hijos
-  const refreshCounters = React.useCallback(() => {
-    setRefreshTrigger((prev) => prev + 1);
-  }, []);
+  // const refreshCounters = React.useCallback(() => {
+  //   setRefreshTrigger((prev) => prev + 1);
+  // }, []);
 
   return (
     <div className="m-4 w-full max-w-md">
